e52343d9a8de0490cd58933d4dea9df73b5ca975
Ben Russell <benjaminrussell36@gmail.com>
2020-09-08 19:25:34 -0700

    Add propTypes to TicketList.js

--- src/components/TicketList.js
+++ src/components/TicketList.js
@@ -1,29 +1,12 @@
 import React from "react";
 import Ticket from "./Ticket";
-
-const masterTicketList = [
-  {
-    names: 'Thato and Haley',
-    location: '3A',
-    issue: 'Firebase won\'t save record. Halp.'
-  },
-  {
-    names: 'Sleater and Kinney',
-    location: '4B',
-    issue: 'Prop types are throwing an error.'
-  },
-  {
-    names: 'Imani & Jacob',
-    location: '9F',
-    issue: 'Child component isn\'t rendering.'
-  }
-];
+import PropTypes from "prop-types";
 
 function TicketList(){
   return (
     <React.Fragment>
       <hr/>
-      {masterTicketList.map((ticket, index) =>
+      {props.ticketList.map((ticket, index) =>
         <Ticket names={ticket.names}
           location={ticket.location}
           issue={ticket.issue}
@@ -32,5 +15,9 @@ function TicketList(){
     </React.Fragment>
   );
 }
+
+TicketList.propTypes = {
+  ticketList: PropTypes.array
+};
 
 export default TicketList;
